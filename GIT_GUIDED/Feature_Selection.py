9cdff7680622945b7454ebd071d1880c2cbe1379
Ngonidzashe chirume <ngonidzashechirume@gmail.com>
2026-01-02 16:36:50 +0400

    Delete GIT_GUIDED directory

--- GIT_GUIDED/Feature_Selection.py
+++
@@ -1,7 +0,0 @@
-# Drop non-feature columns
-from filter import african_urls
-
-X = african_urls.drop(columns=['FILENAME', 'URL', 'Domain', 'Title', 'label'])
-
-# Target column
-y = african_urls['label']
